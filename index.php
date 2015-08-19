4c755c84812fae7dadeecec63e74fbe55e982b8c
unknown <rafael.pomar@hotmail.com>
2015-08-18 22:47:40 -0300

    ta com erro

--- index.php
+++ index.php
@@ -6,10 +6,26 @@
 	//class BD extends BancoDados{};
 
 
-	$bd = new mysql ();
+	$bd = new mysql();
 
-	$conn = $bd->GetConn();
+	//$conn = $bd->GetConn();
 
+	$articles = $bd->executaSQL("SELECT * FROM cake.articles");
+
+	//var_dump($articles);
+	//die;
+
+	$line = '';
+
+	while ($obj = $articles->fetch_object()){
+		$line.=$obj->id;
+		$line.=$obj->title;
+		$line.=$obj->body;
+		$line.="<br>";
+
+	}
+
+	echo $line;
 	
 
 
